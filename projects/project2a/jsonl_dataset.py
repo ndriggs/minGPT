25da4c54de64e6e5cb6fd48f5b73a1e027188ba4
Nathaniel Driggs <nathanieldriggs@gmail.com>
2024-11-11 17:26:02 -0700

    added regular denoising

--- projects/project2a/jsonl_dataset.py
+++ projects/project2a/jsonl_dataset.py
@@ -2,6 +2,7 @@ from transformers import AutoTokenizer
 from torch.utils.data import Dataset
 import torch
 import json
+import numpy as np
 
 class JSONLDataset(Dataset):
     def __init__(self, jsonl_file, head=True):
@@ -32,3 +33,26 @@ class JSONLDataset(Dataset):
 
     def get_block_size(self):
         return self.tokenizer.model_max_length
+    
+    def regular_denoising(self, text) :
+        percent_tokens_corrupted = 0.15
+        average_span_length = 3.5
+        tokens = self.tokenizer(text, truncation=True, max_length=978).input_ids
+        num_corrupted_spans = np.round((len(tokens)*percent_tokens_corrupted)/average_span_length).astype(int)
+        span_positions = (len(tokens)/(num_corrupted_spans+1))*np.arange(num_corrupted_spans+1)
+        new_input_tokens = [self.tokenizer('[NLU] ').input_ids[0]]
+        targets = []
+        span_length = 0
+        for i, (span_position1, span_position2) in enumerate(zip(span_positions[:-1], span_positions[1:])) :
+            new_input_tokens.extend(list(tokens[span_position1+span_length:span_position2]))
+            new_input_tokens.append(self.tokenizer(f'<extra_id_{i}>').input_ids[0])
+            targets.append(self.tokenizer(f'<extra_id_{i}>').input_ids[0])
+            span_length = np.random.randint(2,6)
+            targets.extend(list(tokens[span_position2:span_position2+span_length]))
+        new_input_tokens.extend(list(tokens[span_positions[-1]+span_length:]))
+        new_input_tokens.append(self.tokenizer.pad_token_id)
+        sequence = torch.LongTensor(new_input_tokens + targets)
+        targets_start = len(new_input_tokens)
+        return sequence, targets_start
+
+        
