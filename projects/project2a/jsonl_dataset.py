6c510a9a11f61fbd52275fa6e27a7f1885e1b161
Nathaniel Driggs <nathanieldriggs@gmail.com>
2024-11-13 14:11:09 -0700

    added extreme denoising

--- projects/project2a/jsonl_dataset.py
+++ projects/project2a/jsonl_dataset.py
@@ -25,7 +25,14 @@ class JSONLDataset(Dataset):
         return len(self.data)
 
     def __getitem__(self, idx):
-        tokens = self.tokenizer(self.data[idx], padding='max_length', truncation=True, return_tensors="pt").input_ids[0]
+        # tokens = self.tokenizer(self.data[idx], padding='max_length', truncation=True, return_tensors="pt").input_ids[0]
+        denoising_type = np.random.choice(['S', 'R', 'X'], p=[0.5, 0.25, 0.25])
+        if denoising_type == 'S' :
+            tokens, targets_start = self.sequential_denoising(self.data[idx])
+        elif denoising_type == 'R' :
+            tokens, targets_start = self.regular_denoising(self.data[idx])
+        elif denoising_type == 'X' :
+            tokens, targets_start = self.extreme_denoising(self.data[idx])
         return tokens[:-1], tokens[1:]
 
     def get_vocab_size(self):
@@ -34,18 +41,21 @@ class JSONLDataset(Dataset):
     def get_block_size(self):
         return self.tokenizer.model_max_length
     
-    def regular_denoising(self, text) :
-        percent_tokens_corrupted = 0.15
-        average_span_length = 3.5
-        tokens = self.tokenizer(text, truncation=True, max_length=942).input_ids
+    
+    def generic_denoising(self, text:str, percent_tokens_corrupted: float, 
+                          min_span_len:int, max_span_len: int) :
+        average_span_length = (max_span_len - min_span_len) / 2
+        max_tokens = np.round(1014 / (1 + ((2*percent_tokens_corrupted)/average_span_length))).astype(int) # this took some math
+        tokens = self.tokenizer(text, truncation=True, max_length=max_tokens).input_ids
         num_corrupted_spans = np.round((len(tokens)*percent_tokens_corrupted)/average_span_length).astype(int)
         span_starts = np.random.randint(len(tokens), size=num_corrupted_spans)
         span_starts.sort()
-        span_lengths = np.random.randint(2,6, size=num_corrupted_spans)
-        while any([any((span_start > span_starts) & (span_start <= span_starts + span_lengths)) for span_start in span_starts]) :
+        span_lengths = np.random.randint(min_span_len, max_span_len+1, size=num_corrupted_spans)
+        while any([any((span_start > span_starts) & (span_start <= span_starts + span_lengths)) for span_start in span_starts]) \
+             or any(span_starts + span_lengths > len(tokens)) :
             span_starts = np.random.randint(len(tokens), size=num_corrupted_spans)
             span_starts.sort()
-            span_lengths = np.random.randint(2,6, size=num_corrupted_spans)
+            span_lengths = np.random.randint(min_span_len, max_span_len+1, size=num_corrupted_spans)
         new_input_tokens = [self.tokenizer('[NLU] ').input_ids[0]]
         targets = []
         last_span_start = 0
@@ -64,6 +74,14 @@ class JSONLDataset(Dataset):
         targets_start = len(new_input_tokens)
         return sequence, targets_start
     
+    def regular_denoising(self, text) :
+        if np.random.randint(2) == 1 :
+            return self.generic_denoising(text, percent_tokens_corrupted=0.15, 
+                                          min_span_len=2, max_span_len=5)
+        else : 
+            return self.generic_denoising(text, percent_tokens_corrupted=0.15,
+                                          min_span_len=6, max_span_len=10)
+    
     def sequential_denoising(self, text) :
         tokens = self.tokenizer(text, truncation=True, max_length=1020).input_ids
         span_start = np.round(len(tokens)*0.75).astype(int)
@@ -75,6 +93,20 @@ class JSONLDataset(Dataset):
         return sequence, targets_start
     
     def extreme_denoising(self, text) :
-        pass
+        # I used the parameters from Table 1 in the UL2 paper
+        rand_int = np.random.randint(4)
+        if rand_int == 0 :
+            return self.generic_denoising(text, percent_tokens_corrupted=0.5, 
+                                          min_span_len=2, max_span_len=5)
+        elif rand_int == 1 :
+            return self.generic_denoising(text, percent_tokens_corrupted=0.5, 
+                                          min_span_len=6, max_span_len=10)
+        elif rand_int == 2 :
+            return self.generic_denoising(text, percent_tokens_corrupted=0.15, 
+                                          min_span_len=58, max_span_len=70)
+        elif rand_int == 3 :
+            return self.generic_denoising(text, percent_tokens_corrupted=0.5, 
+                                          min_span_len=58, max_span_len=70)
+        
 
 
